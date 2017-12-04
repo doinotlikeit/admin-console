e204d7cba95c7eb4c4db462242863ae10e1ee92e
doinotlikeit <doinotlikeit@gmail.com>
2017-12-04 13:01:15 -0500

    Clean up

---
+++ vue-ui/src/auth/auth0-variables.js
@@ -0,0 +1,7 @@
+
+export const AUTH_CONFIG = {
+  clientId: 'JdLZVsWnjYlp7uluYc49Qk6bfEYDzAND',
+  domain: 'itstcb.auth0.com',
+  callbackUrl: 'http://localhost:8080/callback',
+  apiUrl: 'https://itstcb.auth0.com/api/v2/'
+}
