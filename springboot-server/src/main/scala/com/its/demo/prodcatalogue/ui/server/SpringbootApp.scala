7ae2d39c3c32f81bc19b719c9b62d070b5b01354
doinotlikeit <doinotlikeit@gmail.com>
2017-12-04 22:45:12 -0500

    Got Springboot integration working

--- springboot-server/src/main/scala/com/its/demo/prodcatalogue/ui/server/SpringbootApp.scala
+++ springboot-server/src/main/scala/com/its/demo/prodcatalogue/ui/server/SpringbootApp.scala
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent
+import org.springframework.boot.web.servlet.ErrorPage
 import org.springframework.context.annotation.{ComponentScan, Configuration}
 import org.springframework.context.{ApplicationEvent, ApplicationListener}
 
@@ -11,6 +12,26 @@ import org.springframework.context.{ApplicationEvent, ApplicationListener}
 @ComponentScan(basePackages = Array("com.its"))
 @Configuration
 class SpringbootApp {
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  import org.springframework.boot.context.embedded.{ConfigurableEmbeddedServletContainer, EmbeddedServletContainerCustomizer}
+  import org.springframework.context.annotation.Bean
+  import org.springframework.http.HttpStatus
+
+  /**
+    * Return the index.html to handle the /callback redirect from Auth0 on successful authentication
+    * @see https://router.vuejs.org/en/essentials/history-mode.html
+    * @return
+    */
+  @Bean def containerCustomizer: EmbeddedServletContainerCustomizer = new EmbeddedServletContainerCustomizer() {
+    override def customize(container: ConfigurableEmbeddedServletContainer): Unit = {
+      //val error401Page = new Nothing(HttpStatus.UNAUTHORIZED, "/401.html")
+      val error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html")
+      //val error500Page = new Nothing(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html")
+      // container.addErrorPages(error401Page, error404Page, error500Page)
+      container.addErrorPages(error404Page)
+    }
+  }
 }
 
 object SpringbootApp extends App {
